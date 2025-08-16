f056c3a76a08d75d58ed8cc4e2e9833174b866b8
perrosovietico34 <142807167+perrosovietico34@users.noreply.github.com>
2025-08-16 19:49:37 -0400

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    burger.classList.toggle("active");
+  });
+});
+
